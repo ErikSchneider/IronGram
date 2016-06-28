5621cc47d46dbf61f5673dfdefff4cb1e4ff3924
Erik <eschneider1128@gmail.com>
2016-06-28 11:30:14 -0400

    added controllers, entities, and services

---
+++ src/main/java/com/theironyard/services/UserRepository.java
@@ -0,0 +1,12 @@
+package com.theironyard.services;
+
+import com.theironyard.entities.User;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by Erik on 6/28/16.
+ */
+public interface UserRepository extends CrudRepository <User, Integer> {
+    public User findFirstByName(String Name);
+
+}
